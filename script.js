ec9f887f70fb6e9226c6611144614dee1646132a
Lazola2 <lazolamakubalo271@gmail.com>
2022-10-04 14:29:43 +0200

    added close button for the side nav

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const btnOpenSideNav = document.querySelector("#side-nav-opener");
 const navbar = document.querySelector("#side-nav");
+const btnCloseSideNav = document.querySelector("#btnCloseSideNav");
 
 btnOpenSideNav.addEventListener('click', () => {
    navbar.style.left = "0px";
-})
+});
+
+btnCloseSideNav.addEventListener('click', () => {
+    navbar.style.left = "-300px";
+ });
